49c9842d06ecf245a7465919f87dd0206232b4f1
chanapon chuenchuwit <63070028@kmitl.ac.th>
2024-06-17 01:22:04 +0700

    account menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import {HomeComponent} from "./home/home.component";
 import {RouterLink, RouterOutlet} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, RouterOutlet, RouterLink],
+  imports: [HomeComponent, RouterOutlet, RouterLink, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-app-reskill';
+
+  isHoverAccountMenu = false;
+
 }
